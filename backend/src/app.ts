8462d05bc08f082e4b3a606573141120b18d1251
Peter Boone <boonepeterg@gmail.com>
2020-06-19 15:51:42 -0400

    'deploy'

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -30,6 +30,7 @@ mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 
 
 app.use(cors())
+app.use(express.static('build'))
 app.use(express.json())
 app.use(middleware.requestLogger)
 app.use(middleware.tokenExtractor)
