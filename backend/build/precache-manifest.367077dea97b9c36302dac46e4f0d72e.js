a002c05cd84b0d317867f3f4de3cef0553fcde20
Peter Boone <boonepeterg@gmail.com>
2020-06-19 15:54:25 -0400

    'deploy'

---
+++ backend/build/precache-manifest.367077dea97b9c36302dac46e4f0d72e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cbe5d8255d5e0909bebaac3b87d6da8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cf91bf94ce7d0331402f",
+    "url": "/static/css/main.43ae5cf4.chunk.css"
+  },
+  {
+    "revision": "d6cc0ee55734f7079889",
+    "url": "/static/js/2.6fbb5430.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.6fbb5430.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf91bf94ce7d0331402f",
+    "url": "/static/js/main.d4172d8f.chunk.js"
+  },
+  {
+    "revision": "93748b38bd80d37ff0ae",
+    "url": "/static/js/runtime-main.19c08194.js"
+  }
+]);
