4ca855cb4d15c997f99220752c0ed357edb5024a
boonepeter <boonepeterg@gmail.com>
2020-09-02 00:51:34 -0400

    update url

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -6,8 +6,8 @@ if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
     apiBaseUrl = 'http://localhost:3003/api'
     baseUrl = 'http://localhost:3001';
 } else {
-    apiBaseUrl = 'https://some-recipes.azurewebsites.net/api';
-    baseUrl = 'https://some-recipes.azurewebsites.net';
+    apiBaseUrl = 'https://some-recipes.com/api';
+    baseUrl = 'https://some-recipes.com';
 }
 
 export const supportedUrls = [
