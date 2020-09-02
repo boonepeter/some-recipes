f1be4cc0a8440740491d0f60c8e54d72816f227a
boonepeter <boonepeterg@gmail.com>
2020-09-02 14:17:37 -0400

    fixed?

--- backend/src/services/pictureService.ts
+++ backend/src/services/pictureService.ts
@@ -1,13 +1,7 @@
-import azure, { BlobService } from "azure-storage";
-import { BlobServiceClient, newPipeline } from "@azure/storage-blob";
+import azure from "azure-storage";
 import Jimp from "jimp";
 import getStream from "into-stream";
-
-import * as dotenv from "dotenv";
-dotenv.config();
-
-const blobService = azure.createBlobService();
-
+import config from "../utils/config";
 
 
 const resizeAndCrop = async (buffer: Buffer): Promise<Jimp|undefined> => {
@@ -23,6 +17,7 @@ const resizeAndCrop = async (buffer: Buffer): Promise<Jimp|undefined> => {
 }
 
 const uploadToBlob = async (image: Jimp, container: string, blob: string): Promise<string|undefined> => {
+    const blobService = azure.createBlobService(config.AZURE_STORAGE_CONNECTION_STRING as string);
     const buffer = await image.getBufferAsync(Jimp.MIME_PNG);
     const stream = getStream(buffer);
     blobService.createBlockBlobFromStream(container, blob, stream, buffer.length,
@@ -50,39 +45,4 @@ export const saveProfilePicture = async (username: string, containerName: string
     if (image) {
         return await uploadToBlob(image, containerName, blobName);
     }
-}
-
-export const createBlob = async (containerName: string) => {
-    /*
-    const conString = process.env.AZURE_STORAGE_CONNECTION_STRING || "";
-    const blobServiceClient = BlobServiceClient.fromConnectionString(conString);
-    for await (const container of blobServiceClient.listContainers()) {
-        console.log(`Container ${container.name}`);
-    }*/
-    console.log("creating service");
-    var blobService = azure.createBlobService();
-    console.log('2');
-    blobService.createContainerIfNotExists(containerName, {
-        publicAccessLevel: 'blob'
-    }, function (error, result, response) {
-        if (!error) {
-            console.log('3');
-            console.log(result);
-        } else {
-            console.log(error.message);
-        }
-
-    });
-
-    console.log('adding file');
-    blobService.createBlockBlobFromLocalFile(containerName, "text/text1.txt", "text.txt", 
-        function (error, result, response) {
-            if (!error) {
-                console.log("uploaded");
-                return "uploaded";
-            } else {
-                console.log(error.message);
-            }
-        }
-    )
 }
--- backend/src/utils/config.ts
+++ backend/src/utils/config.ts
@@ -4,9 +4,11 @@ config();
 
 let PORT = process.env.PORT || 3001;
 let MONGODB_URI = process.env.MONGODB_URI
+let AZURE_STORAGE_CONNECTION_STRING = process.env.AZURE_STORAGE_CONNECTION_STRING;
 
 console.log('mongo uri', MONGODB_URI);
 export default {
     MONGODB_URI,
-    PORT
+    PORT,
+    AZURE_STORAGE_CONNECTION_STRING
 }
